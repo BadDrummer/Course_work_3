c9c789ec9486c721d49d1f42b16c99b477731126
Максим <mr.maks31@mail.ru>
2023-07-30 23:26:51 +0300

    created filter_data_by_executed function in utils.py

--- utils.py
+++ utils.py
@@ -7,3 +7,15 @@ def get_all_operations():
     with open("operations.json", encoding='utf-8') as file:
         data = json.load(file)
     return data
+
+
+'''функция, которая выбирает из исходных данных операции, у которых state = executed'''
+
+
+def filter_data_by_executed(operations: list):
+    result = []
+    for operation in operations:
+        state = operation.get('state')
+        if state and state.lower() == 'executed':
+            result.append(operation)
+    return result

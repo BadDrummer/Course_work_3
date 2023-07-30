8911c15548af29ea934dd497c1186c87a3f54df6
Максим <mr.maks31@mail.ru>
2023-07-30 23:57:52 +0300

    created mask_card function in utils.py

--- utils.py
+++ utils.py
@@ -19,3 +19,14 @@ def filter_data_by_executed(operations: list):
         if state and state.lower() == 'executed':
             result.append(operation)
     return result
+
+
+'''функция для маскировки номера карты или счёта'''
+
+
+def mask_card(card: str):
+    card = card.split(' ')
+    if card[0].lower() == 'счет':
+        return f'{card[0]} **{card[-1][-4:]}'
+    return f'{" ".join(card[:-1])} {card[-1][:4]} {card[-1][4:6]}** **** {card[-1][-4:]}'
+
